3262003e4a2deb340a24572fe8ab5b0d26258128
Kristian <kristianint@outlook.com>
2021-03-26 18:18:52 +0100

    ferdig første utkast

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 const createError = require('http-errors');
 const express = require('express');
+const serveIndex = require('serve-index')
 const ejs = require('ejs');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const alasql = require('alasql');
+
+const adminPass = '8665991a2cff1998';
+const panelToken = '161e3db7b94020a9';
+const formPassword = 'qwerty';
+
+const csv = {
+  by: 'kristiansund',
+  egg: 2652
+};
+
+const flagg = [
+  'EGG0{a6c4ae535de6c632}',
+  'EGG1{b18ce50433b1c112}',
+  'EGG2{32026cc5b03aaca0}',
+  'EGG3{34e0ea28d34bf146}',
+  'EGG4{58f42a65a52cfc26}',
+  'EGG5{0f4e82c54eeb86e0}',
+  'EGG6{507f00b2eb5870a8}',
+]
+
+const panel2Options = {
+  'password': formPassword,
+  token: panelToken,
+  flagg: flagg[5]
+};
+
+
+// setup db
+alasql('CREATE TABLE users (id, INT, username TEXT, password TEXT)');
+alasql.tables.users.data = [
+  { id: 1, username: 'påskeharen', password: adminPass }
+];
 
 const app = express();
 
@@ -20,20 +54,87 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/haredata', serveIndex(path.join(__dirname, 'public/haredata'), { 'icons': true }));
 
-const render = async (res, file, options) => {
-  let out = await ejs.renderFile('./views/header.ejs', options);
+const render = async (res, file, options = {}) => {
+  options = { ...options };
+  options.flagg = options.flagg || flagg[0];
+  let out = '';
+  //out += await ejs.renderFile('./views/header.ejs', options);
   out += await ejs.renderFile(`./views/${file}.ejs`, options);
-  out += await ejs.renderFile('./views/footer.ejs', options);
+  //out += await ejs.renderFile('./views/footer.ejs', options);
   res.send(out);
 }
 
 // BEGIN ROUTES
-app.get('/', function (req, res, next) {
-  render(res, 'index', { title: 'Påskeharens admin-side', sitat: 'Hei hei! Jeg er påskeharen, og dette er min egne nettside! Her holder jeg kontroll over alt arbeidet jeg må gjøre nå i påsketider.' });
+app.get('/', (req, res, next) => {
+  render(res, 'index');
 });
-app.get('/login', function (req, res, next) {
-  render(res, 'login', { title: 'Logg inn', sitat: 'Ånei.. Det er så lenge siden sist påske! Jeg har glemt hva passordet mitt er! Kan du hjelpe meg med å logge inn?' });
+
+app.get('/login', (req, res, next) => {
+  render(res, 'login');
+});
+app.post('/login', (req, res, next) => {
+  if (!req.body.username) {
+    render(res, 'login', { error: 'Missing field username' });
+    return;
+  }
+  if (!req.body.password) {
+    render(res, 'login', { error: 'Missing field password' });
+    return;
+  }
+  alasql([`SELECT id FROM users WHERE username='${req.body.username}' AND password='${req.body.password}'`])
+    .then(user => {
+      if (user) {
+        res.cookie('auth2020', 'my login cookie');
+        res.redirect('/panel');
+      } else {
+        render(res, 'login', { error: `Invalid username or password` });
+      }
+    })
+    .catch(error => {
+      render(res, 'login', { error });
+    });
+});
+
+app.get('/panel', (req, res, next) => {
+  if (req.query.token != panelToken) {
+    if (!req.cookies.auth2021) {
+      render(res, 'errorPretty', { error: `Missing cookie 'auth2021'`, flagg: flagg[1] });
+      return;
+    }
+    render(res, 'panel', { flagg: flagg[2] });
+  } else {
+    render(res, 'panel2', { ...panel2Options });
+  }
+});
+
+app.post('/panel', (req, res, next) => {
+  if (req.query.token != panelToken) {
+    if (!req.body.by) {
+      render(res, 'panel', { error: 'Missing field by' });
+      return;
+    }
+    if (!req.body.egg) {
+      render(res, 'panel', { error: 'Missing field egg' });
+      return;
+    }
+    if (!String(req.body.by).toLowerCase() == csv.by || req.body.egg != csv.egg) {
+      render(res, 'panel', { error: 'Incorrect values' });
+      return;
+    }
+    res.redirect(`/panel?token=${panelToken}`);
+  } else {
+    if (String(req.body.bekreft).toLowerCase() != 'ja') {
+      render(res, 'panel2', { error: 'bekreft != ja, kunne ikke bekrefte utsendelse.', ...panel2Options });
+      return;
+    }
+    if (req.body.passord != formPassword) {
+      render(res, 'panel2', { error: 'Feil passord.', ...panel2Options });
+      return;
+    }
+    render(res, 'ferdig', { flagg: flagg[6] });
+  }
 });
 // END ROUTES
 
